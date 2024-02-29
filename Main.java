6e6885663bf39fa8d9c06e8c25c1929b2601c202
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-02-29 11:02:58 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
-	public static void main(String[] args){
-
-	}
+	public static void main(String[] args) {
+	int num1 = 3, num2 = 5, soma;
+	soma = num1 + num2;
+     }
 }
+	
+
         
