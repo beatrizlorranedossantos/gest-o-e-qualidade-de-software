e53970b649794e6dd02ed1ca97aab91ff246e1cf
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-04-17 18:47:27 -0300

    Create Exercicio3)c.java
    Signed-off-by: beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>

---
+++ Prática 3/Exercicio3)c.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+else if (a == 0 && b != 0) {
+            System.out.println("Caso c) - Esta é uma equação de primeiro grau: x = " + (-c / b));
+        }
