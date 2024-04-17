0903ddc42d46380159765b3d525a88eb3f3b88e0
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-04-17 18:46:35 -0300

    Create Exercicio3)b.java
    Signed-off-by: beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>

---
+++ Prática 3/Exercicio3)b.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       else if (a == 0 && b == 0 && c != 0) {
+            System.out.println("Caso b) - Coeficientes informados incorretamente");
+        }
