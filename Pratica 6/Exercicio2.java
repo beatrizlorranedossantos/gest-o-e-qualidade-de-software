efcc979508887946410e721b7743296d85f409cb
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-05-01 20:47:40 -0300

    Update Exercicio2.java
    Signed-off-by: beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>

--- Pratica 6/Exercicio2.java
+++ Pratica 6/Exercicio2.java
@@ -5,20 +5,21 @@ public class CalculoFatorial {
     public static void main(String[] args) {
         calcularFatorial();
     }
-
+  //Solicitar o número ao usuário
     public static void calcularFatorial() {
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
         
+ // Verificar se o número é válido  
         if (numero < 0) {
             JOptionPane.showMessageDialog(null, "Número inválido.");
             return;
         }
-
+ //Calcular o fatorial do número
         int fatorial = 1;
         for (int i = 1; i <= numero; i++) {
             fatorial *= i;
         }
-
+   //Exibir o resultado para o usuário
         JOptionPane.showMessageDialog(null, "O fatorial de " + numero + " é: " + fatorial);
     }
 }
