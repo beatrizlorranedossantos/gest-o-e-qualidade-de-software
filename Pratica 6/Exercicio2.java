d6e85519a5f751bb6287e7a4e32daa4d5a2cab84
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-05-01 21:23:10 -0300

    Update Exercicio2.java
    Signed-off-by: beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>

--- Pratica 6/Exercicio2.java
+++ Pratica 6/Exercicio2.java
@@ -1,25 +1,36 @@
+// Importa a classe JOptionPane do pacote javax.swing, que permite a exibição de caixas de diálogo
 import javax.swing.JOptionPane;
 
+// Declaração da classe CalculoFatorial
 public class CalculoFatorial {
 
+    // Método principal que inicia a execução do programa
     public static void main(String[] args) {
+        // Chama o método calcularFatorial
         calcularFatorial();
     }
-  //Solicitar o número ao usuário
+  
+    // Método que solicita um número ao usuário e calcula o fatorial desse número
     public static void calcularFatorial() {
+        // Solicita ao usuário que insira o número e o converte para inteiro
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
         
- // Verificar se o número é válido  
+        // Verifica se o número é negativo
         if (numero < 0) {
+            // Exibe uma mensagem de erro se o número for negativo e encerra o método
             JOptionPane.showMessageDialog(null, "Número inválido.");
             return;
         }
- //Calcular o fatorial do número
+        
+        // Inicializa a variável fatorial com 1
         int fatorial = 1;
+        
+        // Calcula o fatorial do número
         for (int i = 1; i <= numero; i++) {
-            fatorial *= i;
+            fatorial *= i; // Multiplica fatorial pelo valor de i em cada iteração
         }
-   //Exibir o resultado para o usuário
+        
+        // Exibe o resultado para o usuário em uma caixa de diálogo
         JOptionPane.showMessageDialog(null, "O fatorial de " + numero + " é: " + fatorial);
     }
 }
